821d4bacdbb259fc5cf396c09d6413dd8ca96003
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-22 01:37:47 +0300

    Enhance TypeScript configuration and Vite setup with path aliases

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
         additionalData: `@use "vuetify/styles" as *;`
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
