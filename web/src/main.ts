bfa5728e440e1635f9eec76d63b7db7a642e128c
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-23 02:52:31 +0300

    Update main.ts to include Vuetify styles import for improved styling consistency

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,7 @@ import router from './router'
 
 // Vuetify
 import 'vuetify/styles'
+// import "vuetify/lib/styles/main.css";
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
