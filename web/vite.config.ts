36ad12d5c3dae28814183e47b8c14d7915d6e912
Ivan Monastyrev <ikloster@yandex.ru>
2025-01-23 02:48:16 +0300

    Refactor Vite configuration to include Vuetify plugin and streamline SCSS setup. Updated path import syntax for consistency.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import * as path from 'path'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "vuetify/styles" as *;`
-      }
-    }
-  },
+  plugins: [vue(), vuetify({ autoImport: true }),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
